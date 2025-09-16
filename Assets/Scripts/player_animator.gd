8659fc88a0925a137bfa9614ae049cb0a11e180b
caitywu <caiwu993@student.liu.se>
2025-09-16 14:48:04 +0200

    changed area and added running animation

--- Assets/Scripts/player_animator.gd
+++ Assets/Scripts/player_animator.gd
@@ -1,8 +1,8 @@
 extends Node2D
 
 @export var player_controller : PlayerController
-@export var animation_player : AnimationPlayer
-@export var sprite : AnimatedSprite2D
+#@export var animation_player : AnimationPlayer
+@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 func _process(_delta):
 	if player_controller.direction == 1:
@@ -11,6 +11,6 @@ func _process(_delta):
 		sprite.flip_h = true
 		
 	if abs(player_controller.velocity.x) > 0.0:
-		animation_player.play("run")
+		sprite.play("run")
 	else:
-		animation_player.play("idle")
+		sprite.play("idle")
