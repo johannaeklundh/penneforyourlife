3c77b789747e4ac97d46089a551c39420477dfb3
caitywu <caiwu993@student.liu.se>
2025-09-18 09:15:15 +0200

    added doublejump

--- Assets/Scripts/player_controller.gd
+++ Assets/Scripts/player_controller.gd
@@ -3,6 +3,7 @@ class_name PlayerController
 
 @export var speed = 10.0
 @export var jump_power = 10.0
+@export var max_jumps := 2   # <-- how many jumps allowed (2 = double jump)
 
 var speed_multiplier = 30.0
 var jump_mulitplier = -30.0
@@ -10,6 +11,7 @@ var jump_mulitplier = -30.0
 var direction = 0.0
 #const SPEED = 300.0
 #const JUMP_VELOCITY = -400.0
+var jumps_left := max_jumps
 
 
 func _physics_process(delta: float) -> void:
@@ -17,12 +19,17 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = jump_power *jump_mulitplier
+	# Reset jumps when touching the floor
+	if is_on_floor():
+		jumps_left = max_jumps
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and jumps_left > 0:
+		velocity.y = jump_power *jump_mulitplier
+		jumps_left -= 1
+
+
+	# Handle movement
 	direction = Input.get_axis("move_left", "move_right")
 	if direction:
 		velocity.x = direction * speed * speed_multiplier
