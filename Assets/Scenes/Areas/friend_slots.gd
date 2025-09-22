6c12dcb278865764b2c1a8003778f3cb8ebb2ab4
caitywu <caiwu993@student.liu.se>
2025-09-22 15:11:39 +0200

    friend flying to overlay + moved camera

--- Assets/Scenes/Areas/friend_slots.gd
+++ Assets/Scenes/Areas/friend_slots.gd
@@ -2,16 +2,46 @@ extends Control
 
 @onready var slots = [$Slot1, $Slot2, $Slot3] # Your target slots
 @onready var glitter = [$Glitter1, $Glitter2, $Glitter3] 
+@onready var camera: Camera2D = $"../../Player/Camera2D"
 
-func _on_bacon_freed_friend(_friend: Variant) -> void:
-	slots[0].set_visible(true)
-	glitter[0].emitting = true
-	
-func _on_tomato_freed_friend(_friend: Variant) -> void:
-	slots[1].set_visible(true)
-	glitter[1].emitting = true
+func _on_bacon_freed_friend(friend: Node2D) -> void:
+	animate_friend_to_slot(friend, 0)
 
-func _on_broccoli_freed_friend(_friend: Variant) -> void:
-	slots[2].set_visible(true)
-	glitter[2].emitting = true
+func _on_tomato_freed_friend(friend: Node2D) -> void:
+	animate_friend_to_slot(friend, 1)
+
+func _on_broccoli_freed_friend(friend: Node2D) -> void:
+	animate_friend_to_slot(friend, 2)
+
+func animate_friend_to_slot(friend: Node2D, index: int) -> void:
+	if not camera:
+		print("❌ Camera not found!")
+		return
+
+	# Make a tiny sprite copy of the freed friend
+	var anim_sprite = friend.get_node("AnimatedSprite2D")
+	var sprite = Sprite2D.new()
+	sprite.texture = anim_sprite.sprite_frames.get_frame_texture("idle", 0)
+	sprite.scale = Vector2(0.25, 0.25) # make smaller
+	add_child(sprite)
+
+	var world_pos: Vector2 = friend.global_position
+	var screen_pos: Vector2 = get_viewport().get_canvas_transform() * world_pos
+
+	var local_pos: Vector2 = get_global_transform_with_canvas().affine_inverse() * screen_pos
 	
+
+
+	sprite.position = local_pos
+
+
+	# Tween to HUD slot
+	var tween = create_tween()
+	tween.tween_property(sprite, "position", slots[index].position, 0.9).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
+
+	#tween.tween_property(sprite, "global_position", slots[index].global_position, 0.8)
+	tween.finished.connect(func():
+		sprite.queue_free()
+		slots[index].visible = true
+		glitter[index].emitting = true
+	)
