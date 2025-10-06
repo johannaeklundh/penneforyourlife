a16ea266dd691dac1fa2cb2b86c41c0eec0b4426
caitywu <caiwu993@student.liu.se>
2025-10-06 14:22:26 +0200

    small fixes

--- Assets/Scripts/player_controller.gd
+++ Assets/Scripts/player_controller.gd
@@ -78,7 +78,7 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_pressed("down"):
 		if is_on_floor():
 			set_collision_mask_value(1, false)  # anta att layer 1 är plattformarna
-			await get_tree().create_timer(0.3).timeout
+			await get_tree().create_timer(0.5).timeout
 			set_collision_mask_value(1, true)
 	# -------------------------------------
 
