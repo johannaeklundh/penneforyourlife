8de1a4594a6a3078898f1ea89f1d2c26127f1b1c
caitywu <caiwu993@student.liu.se>
2025-10-08 15:33:20 +0200

    Update scoreboard.gd

--- Assets/Scripts/scoreboard.gd
+++ Assets/Scripts/scoreboard.gd
@@ -2,15 +2,6 @@ extends Node2D
 
 @export var digit_textures: Array[Texture2D]    # Assign 0–9 handwritten PNGs
 
-#@onready var lines = [
-	#$Score,
-	#$Boiled/Label,
-	#$Flushed/Label,
-	#$Bounced/Label,
-	#$TimeFinish/Label,
-	#$TotalScore
-#]
-
 @onready var lines = [
 	$Score,
 	$Boiled,
@@ -20,10 +11,8 @@ extends Node2D
 	$TotalScore
 ]
 
-#@onready var time_value = $TimeFinish/Value
 @onready var title_label = $Score
 @onready var flushed_label = $Flushed
-var delay := 0.0
 
 @onready var boiled_digits = [
 	$Boiled/Value/digit1,
@@ -56,15 +45,11 @@ var delay := 0.0
 
 func play():
 	visible = true
-	_ready()  # or call fade/animation setup manually
+	_ready() 
 
 func _ready():
 
-	
-	#show_number(boiled_digits, 510)
-	#fade_in_text(title_label)
-	#await get_tree().create_timer(2.0).timeout
-	show_number(boiled_digits, 4)
+	show_number(boiled_digits, 01)
 	show_number(flushed_digits, 52)
 	show_number(bounced_digits, 10)
 	show_number(time_digits, 154)
@@ -72,18 +57,13 @@ func _ready():
 	
 	for line in lines:
 		line.modulate.a = 0.0
-		
-	#await get_tree().create_timer(2.0).timeout
 
 	for line in lines:
 		await get_tree().create_timer(0.5).timeout
 		var tween := get_tree().create_tween()
 
 		tween.tween_property(line, "modulate:a", 1.0, 1.5)
-		
-	#fade_in_text(flushed_label)
-	#animate_scoreboard()
-	
+			
 
 func fade_in_text(line: Node2D):
 	line.modulate.a = 0.0  # start invisible
@@ -110,54 +90,3 @@ func show_number(digits: Array, number: int) -> void:
 			node.visible = true
 		else:
 			node.visible = false
-
-#func animate_scoreboard():
-	#var delay := 0.0
-	#for line in lines:
-		#var value_node = line["node"].Value
-		#fade_in(line["node"].Label, delay)
-		#fade_in(value_node, delay + 0.2)
-		##tick_up_number_label(value_node, line["target"], delay + 0.3)
-		#delay += 1.2
-#
-	## Time
-	#fade_in($TimeFinish.Label, delay)
-	#fade_in(time_value, delay + 0.2)
-	##tick_up_number_label(time_value, 56, delay + 0.3)
-	#delay += 1.5
-#
-	## Total
-	#fade_in($Total.Label, delay)
-	#fade_in($Total.Value, delay + 0.3)
-	##tick_up_total_digits(total_digits, 789, delay + 0.5)
-#
-#
-#func fade_in(node: CanvasItem, delay: float):
-	#var tween = get_tree().create_tween()
-	#tween.tween_property(node, "modulate:a", 1.0, 0.5).set_delay(delay)
-
-#
-#func tick_up_number_label(label: Label, target_value: int, delay: float):
-	#var tween = get_tree().create_tween()
-	#var duration = 1.2
-	#tween.set_delay(delay)
-	#tween.tween_method(func(v):
-		#label.text = str(round(v))
-	#, 0.0, float(target_value), duration)
-#
-#
-#func tick_up_total_digits(digits: Array, target_value: int, delay: float):
-	#var tween = get_tree().create_tween()
-	#var duration = 2.0  # take a bit longer for the big reveal
-	#tween.set_delay(delay)
-	#tween.tween_method(func(v):
-		#var current = int(round(v))
-		#update_digits(digits, current)
-	#, 0.0, float(target_value), duration)
-#
-#
-#func update_digits(digits: Array, number: int):
-	#var str_num = str(number).pad_zeroes(digits.size())
-	#for i in digits.size():
-		#var digit = int(str_num[i])
-		#digits[i].texture = digit_textures[digit]
