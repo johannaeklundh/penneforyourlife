626de478777d81c6f83b187ac5b8d20debd8775b
caitywu <caiwu993@student.liu.se>
2025-10-08 10:59:47 +0200

    filled+lined pasta + different jumpcloud

--- Assets/Scripts/puff.gd
+++ Assets/Scripts/puff.gd
@@ -2,12 +2,12 @@ extends Sprite2D
 
 func _ready():
 	# Start small
-	scale = Vector2(0.04, 0.04)
+	scale = Vector2(0.06, 0.06)
 	modulate = Color(1, 1, 1, 1)
 
 	var tween = create_tween()
 	# Scale up to "poof"
-	tween.tween_property(self, "scale", Vector2(0.02, 0.02), 0.1)
+	tween.tween_property(self, "scale", Vector2(0.04, 0.04), 0.1)
 	# Fade out
 	tween.tween_property(self, "modulate:a", 0.0, 0.3).set_delay(0.05)
 	# When done, remove puff
